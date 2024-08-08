b6c49549a17823be6dd17d4edbc6035c37344194
Rakesh60 <rakeshgupta5353@gmail.com>
2024-08-09 00:20:57 +0530

    image not comming

--- src/main/java/com/vendor/controller/AdminController.java
+++ src/main/java/com/vendor/controller/AdminController.java
@@ -9,6 +9,7 @@ import java.nio.file.StandardCopyOption;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -25,33 +26,37 @@ import jakarta.servlet.http.HttpSession;
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
-	
+
 	@Autowired
 	private CategoryService categoryService;
-	
+
 	@GetMapping("/")
 	public String index() {
-		
+
 		return "admin/index";
 	}
+
 	@GetMapping("/loadProducts")
 	public String addProducts() {
-		
+
 		return "admin/add-products";
 	}
+
 	@GetMapping("/category")
-	public String Category() {
-		
+	public String category(Model m) {
+		m.addAttribute("categories", categoryService.getAllCategory());
+
 		return "admin/add-category";
 	}
-	
+
 	@PostMapping("/savecategory")
-	public String saveCategory(@ModelAttribute Category category,@RequestParam("file") MultipartFile file ,HttpSession session) throws IOException {
-		
-		String imageName=file!=null?file.getOriginalFilename():"default.jpg";
+	public String saveCategory(@ModelAttribute Category category, @RequestParam("file") MultipartFile file,
+			HttpSession session) throws IOException {
+
+		String imageName = file != null ? file.getOriginalFilename() : "default.jpg";
 		category.setImagename(imageName);
-		
-		Boolean existCategory=categoryService.existCategory(category.getName());
+
+		Boolean existCategory = categoryService.existCategory(category.getName());
 		if (existCategory) {
 			session.setAttribute("errorMsg", "Categry Already exist");
 		} else {
@@ -62,23 +67,22 @@ public class AdminController {
 				// Define the path where you want to save the file
 				String uploadDir = "uploads/img/category_img";
 				File uploadDirectory = new File(uploadDir);
-	
+
 				// Create directories if they don't exist
 				if (!uploadDirectory.exists()) {
 					uploadDirectory.mkdirs();
 				}
-	
+
 				// Save the file
 				Path filePath = Paths.get(uploadDirectory.getAbsolutePath(), file.getOriginalFilename());
 				Files.copy(file.getInputStream(), filePath, StandardCopyOption.REPLACE_EXISTING);
-	
+
 				System.out.println("File saved to: " + filePath);
-	
+
 				session.setAttribute("successMsg", "Category Saved Successfully");
 			}
 		}
-		
-		
+
 		return "redirect:/admin/category";
 	}
 }
